a746c44a9fa4816081fa14e7752ecad722508969
youvegotnigel <nigeldave19@gmail.com>
2022-12-27 23:40:11 +0530

    Create FormPageStepDefinitions.java

---
+++ src/test/java/com/youvegotnigel/automation/stepdefs/FormPageStepDefinitions.java
@@ -0,0 +1,52 @@
+package com.youvegotnigel.automation.stepdefs;
+
+import com.youvegotnigel.automation.base.TestBase;
+import com.youvegotnigel.automation.pageobjects.forms.FormPage;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.Map;
+
+public class FormPageStepDefinitions extends TestBase {
+
+    public static final Logger log = LogManager.getLogger(FormPageStepDefinitions.class.getName());
+
+    FormPage formPage = new FormPage(eventFiringWebDriver);
+
+
+    @And("I click on Submit form button")
+    public void submit_form() {
+        log.info("Submitting form");
+        formPage.submitForm();
+    }
+
+    @And("Application should display success message {string}")
+    public void verify_success_message_text(String text) {
+        Assert.assertEquals(formPage.getSuccessHeaderText(), text);
+    }
+
+    @And("Application should display form submission values as below:")
+    public void verify_form_submissions(DataTable dataTable) {
+
+        List<Map<String, String>> values = dataTable.asMaps(String.class, String.class);
+
+        values.forEach(
+                map -> map.entrySet().forEach(entry -> {
+                    String key = entry.getKey();
+                    String value = entry.getValue();
+
+                    System.out.println("KEY   ::: " + key);
+                    System.out.println("VALUE ::: " + value);
+
+                    Assert.assertEquals(formPage.getFormSubmissionResponse(key),value);
+
+                })
+        );
+    }
+
+
+}
