6f4cf9d8ee670715b50c0f9eda4be5f24930406b
youvegotnigel <nigeldave19@gmail.com>
2022-12-27 13:36:54 +0530

    Update WebElementHelper.java

--- src/test/java/com/youvegotnigel/automation/utils/newtable/WebElementHelper.java
+++ src/test/java/com/youvegotnigel/automation/utils/newtable/WebElementHelper.java
@@ -1,10 +1,12 @@
 package com.youvegotnigel.automation.utils.newtable;
 
 import com.youvegotnigel.automation.base.TestBase;
+import org.apache.commons.lang.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
@@ -60,6 +62,36 @@ public class WebElementHelper {
 
         }catch (Exception e){
             System.out.printf("Fail to click on element '%s' due to error: %s", element, e.getMessage());
+        }
+    }
+
+    public static void selectOptionByLabel(WebElement element, String labelText, boolean isRegex){
+        try {
+            System.out.printf("Selecting an option by its label. Label='%s'. Using Regular Expression: '%s'", labelText, isRegex);
+            String optionElementXpath = String.format(".//option[normalize-space(text())='%s']", labelText);
+            if(isRegex){
+                String DELIMETER1 ="~!@1";
+                String DELIMETER2 ="~!@2";
+                String DELIMETER3 ="~!@3";
+                String regLabel = DELIMETER1 + labelText + DELIMETER2;
+                String outerHtml = element.getAttribute("outerHTML");
+                outerHtml = outerHtml.replaceAll("</option>", DELIMETER2+DELIMETER3).replaceAll("<option.*?>",DELIMETER2+DELIMETER1).replaceAll("</select>", DELIMETER2+DELIMETER3);
+                String[] match = outerHtml.split(labelText);
+
+                if(match.length<=1){
+                    throw new Exception("Could not find option with label " + labelText + " (regex=true) in select box");
+                }
+
+                int index = StringUtils.countMatches(match[0], DELIMETER1);
+                optionElementXpath = ".//option[" + (index+1) + "]";
+                new Select(element).selectByIndex(index);
+
+            }else{
+                new Select(element).selectByVisibleText(labelText);
+            }
+
+        }catch (Exception e){
+            System.out.printf("Could not find option with label '%s' (regex=%s) in select box due to error: %s", labelText, isRegex, e.getMessage());
         }
     }
 
