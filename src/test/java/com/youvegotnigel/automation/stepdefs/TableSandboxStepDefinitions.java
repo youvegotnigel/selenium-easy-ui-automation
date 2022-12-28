60f38a050145bfdb2132a449533fbbdefe515c33
youvegotnigel <nigeldave19@gmail.com>
2022-12-28 22:02:02 +0530

    Update TableSandboxStepDefinitions.java

--- src/test/java/com/youvegotnigel/automation/stepdefs/TableSandboxStepDefinitions.java
+++ src/test/java/com/youvegotnigel/automation/stepdefs/TableSandboxStepDefinitions.java
@@ -2,6 +2,8 @@ package com.youvegotnigel.automation.stepdefs;
 
 import com.youvegotnigel.automation.base.PageBase;
 import com.youvegotnigel.automation.base.TestBase;
+import com.youvegotnigel.automation.pageobjects.TablePage;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,12 +12,19 @@ public class TableSandboxStepDefinitions extends TestBase {
 
     public static final Logger log = LogManager.getLogger(TableSandboxStepDefinitions.class.getName());
     PageBase pageBase = new PageBase(eventFiringWebDriver);
+    TablePage tablePage = new TablePage(eventFiringWebDriver);
 
     @Given("User has navigated {string} sandbox")
     public void navigate_to_table_sandbox(String page) {
 
         pageBase.clickOnLinkByName(page);
         log.info("Navigating to page ::: " + page);
+    }
+
+    @And("I type {string} in table search")
+    public void table_filer(String text) {
+
+        tablePage.table_search(text);
     }
 
 }
