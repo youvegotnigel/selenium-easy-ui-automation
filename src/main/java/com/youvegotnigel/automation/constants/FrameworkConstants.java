8b09b17a4beb12fca004f385cbaa7558a0318849
youvegotnigel <nigeldave19@gmail.com>
2022-12-29 22:24:52 +0530

    Update FrameworkConstants.java

--- src/main/java/com/youvegotnigel/automation/constants/FrameworkConstants.java
+++ src/main/java/com/youvegotnigel/automation/constants/FrameworkConstants.java
@@ -1,5 +1,13 @@
 package com.youvegotnigel.automation.constants;
 
+/**
+ * Define the framework constants here
+ *
+ * Dec 29, 2022
+ * @author Nigel Mulholland
+ * @version 1.0
+ * @since 1.0
+ */
 public class FrameworkConstants {
 
     /**
@@ -12,14 +20,19 @@ public class FrameworkConstants {
     private static final int EXPLICIT_WAIT = 10;
     private static final String RESOURCES_PATH = System.getProperty("user.dir") + "/src/test/resources";
     private static final String CONFIG_FILE_PATH = RESOURCES_PATH + "/config/config.properties";
+    private static final String CUCUMBER_REPORT_FOLDER_PATH = System.getProperty("user.dir")+"/target/cucumber-reports";
 
 
-    public static int getExplicitwait() {
+    public static int getExplicitWait() {
         return EXPLICIT_WAIT;
     }
 
     public static String getConfigFilePath() {
         return CONFIG_FILE_PATH;
+    }
+
+    public static String getCucumberReportFolderPathPath() {
+        return CUCUMBER_REPORT_FOLDER_PATH;
     }
 
 }
