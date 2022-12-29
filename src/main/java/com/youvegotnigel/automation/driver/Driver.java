d340e2fb4460f68cf563a96fa6b27eb0de071270
youvegotnigel <nigeldave19@gmail.com>
2022-12-29 22:24:49 +0530

    Create Driver.java

---
+++ src/main/java/com/youvegotnigel/automation/driver/Driver.java
@@ -0,0 +1,55 @@
+package com.youvegotnigel.automation.driver;
+
+import com.youvegotnigel.automation.factories.DriverFactory;
+import com.youvegotnigel.automation.utils.PropertyUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.net.MalformedURLException;
+import java.util.Objects;
+
+/**
+ *
+ * Driver class is responsible for invoking and closing the browsers.
+ *
+ * @author Nigel Mulholland
+ * Dec 29, 2022
+ * @version 1.0
+ * @since 1.0
+ */
+public final class Driver {
+
+    public static final Logger log = LogManager.getLogger(Driver.class.getName());
+
+    /**
+     * Private constructor to avoid external instantiation
+     */
+    private Driver() {}
+
+    /**
+     * Gets the browser value and initialise the browser based on that
+     */
+    public static void initDriver(){
+
+        if(Objects.isNull(DriverManager.getDriver())) {
+            try {
+                DriverManager.setDriver(DriverFactory.getDriver());
+            } catch (MalformedURLException e) {
+                log.error("Please check the capabilities of browser");
+            }
+            DriverManager.getDriver().get(PropertyUtils.get("LOGIN_URL"));
+        }
+    }
+
+    /**
+     * Terminates the browser instance.
+     * Sets the thread local to default value, i.e null.
+     */
+    public static void quitDriver() {
+        if(Objects.nonNull(DriverManager.getDriver())) {
+            DriverManager.getDriver().quit();
+            DriverManager.unload();
+        }
+    }
+
+}
